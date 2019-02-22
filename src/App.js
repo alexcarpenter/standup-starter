90cebe8c1d5fb18f6e3ac297935d6b79776a81de
Alex Carpenter <im.alexcarpenter@gmail.com>
2019-02-22 14:42:28 -0500

    Use null instead of empyty string

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     super(props)
     this.state = {
       items: [],
-      selectedItem: '',
+      selectedItem: null,
       confetti: false,
       sidebarVisible: false,
     }
